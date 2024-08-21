d5a0640eb5b2a4ff98736b3ec08096aef1c87a28
Gustavo Medeiros <136112308+Gustavomedeirost@users.noreply.github.com>
2024-08-21 00:03:54 -0300

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-print("Sabor Express\n")
+print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+""")
 
-print("1. Cadastrar restaurante")
-print("2. Listar restaurante")
-print("3. Ativar restaurante")
-print("4. Sair")
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
 
-opcao_escolhida = input("Escolha uma opção: ")
-print("Você escolheu essa opção:", opcao_escolhida)
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção {opcao_escolhida}')
